0be0610637eacfc1711b9d2f8352e7eefc0313bd
Andrew Ozeki <andrewozeki@gmail.com>
2021-07-20 19:05:50 +0900

    updated: countries model to include image to dynamically display in html

--- src/app/components/countries/countries.model.ts
+++ src/app/components/countries/countries.model.ts
@@ -2,5 +2,6 @@ export interface Country {
     id: number;
     name: string;
     homes: number;
-    image?: HTMLImageElement; 
+    price: string;
+    image: string; 
 }
--- src/app/services/data.services.ts
+++ src/app/services/data.services.ts
@@ -12,49 +12,57 @@ export class DataService implements InMemoryDbService {
           id: 1,
           name: 'America',
           homes: 62,
-          price: '$40'
+          price: '$40',
+          image: '/assets/images/america.png'
         },
         {
           id: 2,
           name: 'France',
           homes: 27,
-          price: '$80'
+          price: '$80',
+          image: '/assets/images/france.png'
         },
         {
           id: 3,
           name: 'Japan',
           homes: 21,
-          price: '$30'
+          price: '$30',
+          image: '/assets/images/japan.png'
         },
         {
           id: 4,
           name: 'Italy',
           homes: 14,
-          price: '$30'
+          price: '$30',
+          image: '/assets/images/italy.png'
         },
         {
           id: 5,
           name: 'China',
           homes: 11,
-          price: '$30'
+          price: '$30',
+          image: '/assets/images/china.png'
         },
         {
           id: 6,
           name: 'Thailand',
           homes: 23,
-          price: '$30'
+          price: '$30',
+          image: '/assets/images/thailand.png'
         },
         {
           id: 7,
           name: 'Australia',
           homes: 36,
-          price: '$30'
+          price: '$30',
+          image: '/assets/images/australia.png'
         },
         {
           id: 8,
           name: 'Germany',
           homes: 24,
-          price: '$30'
+          price: '$30',
+          image: '/assets/images/germany.png'
         },
       ]
     };
